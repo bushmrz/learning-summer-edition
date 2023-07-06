fb16493d08aa92d54ad7b6906f7fb94d67c90de4
bushmrzzz <buuushmrz@mail.ru>
2023-07-06 05:25:09 +0300

    initial commit

---
+++ testing/django_crud_project/tests/api/test_exmpl.py
@@ -0,0 +1,21 @@
+import pytest
+from rest_framework.test import APIClient
+
+client = APIClient()
+
+
+@pytest.mark.django_db
+def test_get_note():
+    pass
+
+def test_post_note():
+    pass
+
+
+def test_update_note():
+    pass
+
+
+def test_delete_note():
+    pass
+
